179bec8d0fa89e4e0d171a0330d6eeed6eb0f044
Rodrigo Lima <rodlima@motorola.com>
2023-02-17 06:15:19 -0400

    feat: add error middleware

---
+++ src/backend/middlewares/errorMiddleware.js
@@ -0,0 +1,29 @@
+const errors = {
+  invalidCredentials: {
+    code: 401,
+    message: 'Your coreid or password is invalid.',
+  },
+  forbiddenCredentials: {
+    code: 403,
+    message:
+      'Your login has failed more than two times, you need to make login in Jira Website and pass manually of the security CAPTCHA.',
+  },
+  internalError: {
+    code: 500,
+    message: 'Request failed with status code 500',
+  },
+  invalidKitEntry: {
+    code: 400,
+    message: 'Please review the current svnkit data',
+  },
+};
+
+module.exports = (err, _req, res, _next) => {
+  const error = errors[err] || errors[err.message];
+
+  if (error) {
+    return res.status(error.code).json({ message: error.message });
+  } else {
+    return res.status(500).json(err);
+  }
+};
