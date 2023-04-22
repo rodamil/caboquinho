e30c6345219eeef628f6e938187e4d51fed79f9b
Rodrigo Lima <rodlima@motorola.com>
2023-04-21 21:31:18 -0400

    feat: reorganize some data

--- src/backend/services/dpmService.js
+++ src/backend/services/dpmService.js
@@ -6,46 +6,6 @@ const DPM_ISSUE_TYPE_ID = '13806';
 const COMPONENT_NAME = 'DPM OTA Sw Updates';
 const SW_TYPE = 'streamingOnAb';
 
-const dpmData = {
-  bgGroupColor: '#ED9720',
-  check: '1',
-  summary: 'DEVONF_G - XT2237-1 - Retail Brazil - DS - T1TNS33.14-44-2-1 - T1TN33.14-60',
-  rocarrierField: 'retbr',
-  cds: 'https://moto-cdsp.appspot.com/upgrades#/paths/edit/948c1032-3fb9-47f8-8a02-9adb9a61281f',
-  xmlUrl:
-    'https://artifacts.mot.com/artifactory/devonf_US/13/T1TN33.14-60/devonf_g_sys/user/release-keys_cid50/OTA_FROM_T1TNS33.14-32-4-1_TO_T1TN33.14-60_V1/ab_dp_delta-Ota_Version_devonf_g_sys_T1TNS33.14-32-4-1-T1TN33.14-60_release-keys.xml',
-  productName: 'Devon5G23 Latam DS',
-  model: 'XT2237-1',
-  source: 'T1TNS33.14-44-2-1',
-  target: 'T1TN33.14-60',
-  deivceIdField: ['101204', '101205'],
-  regionName: 'Latin America',
-  launchCountriesField: ['Brazil'],
-  carriersCountriesField: ['Retail - Brazil', 'Claro - Brazil'],
-  launchType: 'MR2',
-  packLocation:
-    'https://artifacts.mot.com/artifactory/devonf_US/13/T1TN33.14-60/devonf_g_sys/user/release-keys_cid50/',
-  packSanity:
-    'https://drive.google.com/drive/folders/1WXb-FMRb7wgdkZkmS_zG9_Muqau5zbqv?usp=share_link',
-  forcedUpgrade: 'FALSE',
-  downdloadWifiOnly: 'TRUE',
-  showPreDownloadMsg: 'TRUE',
-  showDownloadOptions: 'FALSE',
-  botaTextLink:
-    'https://drive.google.com/drive/folders/1kJfZISKNYAsjhU6fwMDomQQ6CTtH33aG',
-  releaseNotesLink: 'https://N/A',
-  md5: '3aa782367934de6393d7dd3837b3944b',
-  packageSize: '978.63',
-  listOfDevices: 'Devon_5G_npi',
-  wbLink:
-    'https://docs.google.com/spreadsheets/d/1wEedGykH3iwCsuLoBgsJ_AMJPNoYnlBWbhcXM6QiY_o/edit#gid=1958962067',
-  productManager: 'aurelioc',
-  technicalLead: 'igoro',
-  rocarrierPlannedField: ['retbr'],
-  isMultiConfig: true,
-  isOdm: false,
-};
-
 const handleXmlData = (xmlString) => {
   const xmlDoc = xml2js(xmlString, { compact: true });
   const buildData = xmlDoc['build_data'];
@@ -93,7 +53,7 @@ const getAndroidVersion = (buildStringNumber) => {
   return `${majorVersion} ${minorVersion}.0`;
 };
 
-async function create(authorization, url) {
+async function create(dpmData, authorization, url) {
   const {
     summary,
     xmlUrl,
@@ -218,18 +178,17 @@ async function create(authorization, url) {
         customfield_25516: formatedProjectClassification,
       },
     };
-    console.log(requestBody);
+
     const dpmResponse = await dpmModel.create(authorization, requestBody, url);
-    console.log(dpmResponse);
 
     return dpmResponse;
   } catch (err) {
-    // if (err.response.status == 400) {
-    //   throw new Error('invalidDpmEntry');
-    // } else {
-    //   throw new Error('internalError');
-    // }
+    if (err.response.status == 400) {
+      throw new Error('invalidDpmEntry');
+    } else {
+      throw new Error('internalError');
+    }
   }
 }
-create('Basic cm9kbGltYTpFbGRAMjMwMg==', 'https://idart-test.mot.com');
+
 module.exports = { create };
